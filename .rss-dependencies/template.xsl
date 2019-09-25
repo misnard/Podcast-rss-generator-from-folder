e953082772191128c020b366667772bee8cfe044
misnard <maher.isnard@gmail.com>
2019-09-25 12:03:05 +0000

    add channel infos and some fixes

---
+++ .rss-dependencies/template.xsl
@@ -0,0 +1,245 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+    <xsl:output method="html" version="1.0" encoding="ISO-8859-1" indent="yes"/>
+    <xsl:template match="/">
+
+        <html xmlns="http://www.w3.org/1999/xhtml">
+            <head>            	
+                <title><xsl:value-of select="/rss/channel/title"/> - RSS</title>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <style type="text/css">
+                	@import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600');
+
+                    body {
+                        font-family: 'Montserrat', sans-serif;
+                        font-size: 14px;
+                        color: #545454;
+                        background: #E5E5E5;
+                        line-height: 1.5;
+                    }
+                    .explanation {
+                        font-style: italic;
+                        font-size: 10px;
+                        color: #9E9E9E;
+                        text-align: center;
+                    }
+                    a, a:link, a:visited {
+                        color: #005C82;
+                        text-decoration: none;
+                    }
+                    a:hover {
+                        color: #000;
+                    }
+                    h1 {
+                   		margin-top: 0;
+                   		 margin-bottom: 0;
+                   		 font-weight: 300;
+                   		 font-size: xx-large;
+                    }
+
+                    h2, h3 {
+                        margin-top: 0;
+                        margin-bottom: 0px;
+                        font-weight:300;
+                    }
+                    h2 {
+                    	margin-top: 20px;
+                    }
+
+                    h3 {
+                    	font-size:small;
+                    	font-weight: 500;
+                    }
+
+                    img {
+                    max-width: 100%;
+                    } 
+                    
+                    #content {
+                        max-width: 900px;
+                        margin: 0 auto;
+                        background: #FFF;
+                        padding: 30px;
+                        border-radius: 1em;
+                        padding-top: 0px;
+                        box-shadow: 0px 0px 2px #5D5D5D;
+                    }
+                    #channel-image {
+                        float: right;
+                        width: 200px;
+                        margin-bottom: 20px;
+                    }
+                    #channel-image img {
+                        width: 200px;
+                        height: auto;
+                        border-radius: 3px;
+                        margin-left: 10px;
+                    }
+                    #channel-header {
+                        margin-bottom: 20px;
+                        padding-top: 20px;
+                        margin-left: -10px;
+                    }
+                    .channel-item {
+                        clear: both;
+                        border-top: 2px solid #E5E5E5;
+                        margin: 10px;
+                        overflow-wrap: break-word;
+  						word-wrap: break-word;
+  						hyphens: auto;
+                    }
+
+                    .episode-image {
+                         float: left;
+   						 width: 100px;
+  						 margin-right: 10px;
+  						 margin-bottom: 10px;
+  					     margin-top: 10px;
+
+                    }
+
+                    .episode-image img {
+                        width: 100px;
+                        height: auto;
+                        border-radius: 5px;
+                    }
+ 					
+                    .episode-title {
+                        margin-bottom:20px;
+                    }
+
+                    .episode_meta {
+                        font-size: 11px;
+                        font-weight: 500;
+                        margin-top: 20px;
+                        margin-bottom: 10px;
+                    }
+                    .channel-description {
+						margin-bottom: 10px;
+						overflow-wrap: break-word;
+  						word-wrap: break-word;
+  						hyphens: auto;
+
+                    }
+                     .channel-copyright {
+                     	 	 text-align: center;
+   							 margin: 10px;
+   							 font-size: small;
+
+                    }
+                     .channel-author {
+   						 font-size: small;
+  					     font-weight: 600;
+  					     margin-bottom: 10px;                    
+                    }
+                     .channel-subtitle {
+   						 font-size: small;
+  					     font-weight: 500;
+  					     margin-bottom: 20px;                    
+                    }
+                </style>
+            </head>
+            <body>
+            	<p class="explanation">
+                        This is a podcast RSS feed generated by <xsl:value-of select="/rss/channel/generator"/>. It is meant for consumption by podcast feed readers using the URL in the address bar.
+                    </p>
+                <div id="content">
+                    
+                    <div id="channel-header">
+                        <h1>
+                            <xsl:if test="/rss/channel/image">
+                                <div id="channel-image">
+                                    <img>
+                                            <xsl:attribute name="src">
+                                                <xsl:value-of select="/rss/channel/image/url"/>
+                                            </xsl:attribute>
+                                            <xsl:attribute name="title">
+                                                <xsl:value-of select="/rss/channel/image/title"/>
+                                            </xsl:attribute>
+                                        </img>                                  
+                                </div>
+                            </xsl:if>
+                            <xsl:value-of select="/rss/channel/title"/>
+                        </h1>
+                        <div class="channel-subtitle">
+                            <xsl:value-of select="/rss/channel/itunes:subtitle" disable-output-escaping="yes"/>
+                        </div>
+                        <div class="channel-description">
+                            <xsl:value-of select="/rss/channel/description" disable-output-escaping="yes"/>
+                        </div>
+                        <div class="channel-author">
+                            <xsl:value-of select="/rss/channel/itunes:author" disable-output-escaping="yes"/>
+ 						<a style="font-size: large;font-weight: 600;">
+                                <xsl:attribute name="href">
+                                    <xsl:value-of select="/rss/channel/link"/>
+                                </xsl:attribute>
+                                <xsl:attribute name="target">_blank</xsl:attribute>
+                                🌐
+                            </a>
+                        </div>
+                        
+                           
+                        
+                    </div>
+                    <xsl:for-each select="/rss/channel/item">
+                        <div class="channel-item">
+                           <div class="episode-title">
+                            <h2>
+                                <a>
+                                    <xsl:attribute name="href">
+                                        <xsl:value-of select="link"/>
+                                    </xsl:attribute>
+                          
+                                    <xsl:attribute name="target">_blank</xsl:attribute>
+                                    <xsl:value-of select="title"/>
+                                </a>
+                                <div class="episode-image">
+                           			 <img>
+                                            <xsl:attribute name="src">
+                                                <xsl:value-of select="itunes:image/@href"/>
+                                            </xsl:attribute>
+                                            <xsl:attribute name="title">
+                                                <xsl:value-of select="title"/>
+                                            </xsl:attribute>
+                           			  </img>
+                         		</div>
+                            </h2>
+                            <h3>
+                         		 <xsl:if test="itunes:author">
+                               		 <xsl:value-of select="itunes:author" disable-output-escaping="yes"/>
+                               	 </xsl:if>
+                         	</h3>
+                           </div>
+                            
+                         	<div class="episode-description">
+                         		
+                            <xsl:if test="description">
+                                <p>
+                                    <xsl:value-of select="description" disable-output-escaping="yes"/>
+                                </p>
+                            </xsl:if>
+                           
+                        	</div>
+                            <p class="episode_meta">                            	   
+                                    <a>
+                                        <xsl:attribute name="href">
+                                            <xsl:value-of select="enclosure/@url"/>?ref=new_window
+                                        </xsl:attribute>
+                                        <xsl:attribute name="target">_blank</xsl:attribute>
+                                        💾                                 
+                                        <xsl:value-of select='format-number(number(enclosure/@length div "1024000"),"0.0")'/>MB
+                                    </a> | ⏱ 
+                                    <xsl:value-of select="itunes:duration" disable-output-escaping="yes"/> | 🗓
+                                    <xsl:value-of select="pubDate" />
+                            </p>
+                        </div>
+                    </xsl:for-each>
+                </div>
+                <div class="channel-copyright">
+                	&#x24B8; <xsl:value-of select="/rss/channel/copyright"/>
+            	</div>
+            </body>
+        </html>
+    </xsl:template>
+</xsl:stylesheet>
+
--- rss.php
+++ rss.php
@@ -1,67 +1,135 @@
 <?php
 Header('Content-type: text/xml');
 
+/**
+ * Class BootstrapGenerator
+ */
 class BootstrapGenerator
 {
+    /**
+     * BootstrapGenerator constructor.
+     */
     public function __construct()
     {
-        if (!file_exists('./.rss-gen-cache')) {
-            mkdir('./.rss-gen-cache');
+        if (!file_exists('./.rss-dependencies/.cache')) {
+            mkdir('./.rss-dependencies/.cache');
         }
 
-        if (!file_exists('./.rss-gen-cache/id3requirements')) {
-            mkdir('./.rss-gen-cache/id3requirements');
+        if (!file_exists('./.rss-dependencies/.cache/id3requirements')) {
+            mkdir('./.rss-dependencies/.cache/id3requirements');
             $file = file_get_contents('https://github.com/misnard/getID3/archive/master.zip');
-            file_put_contents('./.rss-gen-cache/id3requirements/master.zip', $file);
+            file_put_contents('./.rss-dependencies/.cache/id3requirements/master.zip', $file);
 
             $zip = new ZipArchive;
-            if ($zip->open('./.rss-gen-cache/id3requirements/master.zip')) {
-                $zip->extractTo('./.rss-gen-cache/id3requirements/');
+            if ($zip->open('./.rss-dependencies/.cache/id3requirements/master.zip')) {
+                $zip->extractTo('./.rss-dependencies/.cache/id3requirements/');
                 $zip->close();
-                unlink('./.rss-gen-cache/id3requirements/master.zip');
+                unlink('./.rss-dependencies/.cache/id3requirements/master.zip');
             }
         }
 
-        require_once('./.rss-gen-cache/id3requirements/getID3-master/getid3/getid3.php');
+        require_once('./.rss-dependencies/.cache/id3requirements/getID3-master/getid3/getid3.php');
     }
 }
 
-class RssGenerator 
+/**
+ * Class CustomXMLElement
+ */
+class CustomXMLElement extends SimpleXMLElement
 {
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function addProcessingInstruction($name, $value )
+    {
+        // Create a DomElement from this simpleXML object
+        $dom_sxe = dom_import_simplexml($this);
+
+        // Create a handle to the owner doc of this xml
+        $dom_parent = $dom_sxe->ownerDocument;
+
+        // Find the topmost element of the domDocument
+        $xpath = new DOMXPath($dom_parent);
+        $first_element = $xpath->evaluate('/*[1]')->item(0);
+
+        // Add the processing instruction before the topmost element
+        $pi = $dom_parent->createProcessingInstruction($name, $value);
+        $dom_parent->insertBefore($pi, $first_element);
+    }
+}
+
+/**
+ * Class RssGenerator
+ */
+class RssGenerator
+{
+    /**
+     * @var CustomXMLElement
+     */
     protected $rssNode;
+    /**
+     * @var SimpleXMLElement
+     */
     protected $channelNode;
+    /**
+     * @var
+     */
     protected $currentUrl;
+    /**
+     * @var getID3
+     */
     protected $ID3;
+    /**
+     * @var
+     */
     protected $config;
 
+    /**
+     * RssGenerator constructor.
+     */
     public function __construct()
     {
         $this->getConfig();
         $this->ID3 = new getID3;
-        $this->rssNode = new SimpleXMLElement('<rss />');
+        $this->rssNode = new CustomXMLElement('<rss />');
+
+        if ($this->config['THEME_ENABLE']) {
+            $this->rssNode->addProcessingInstruction('xml-stylesheet', 'type="text/xsl" href=".rss-dependencies/template.xsl"');
+        }
+
         $this->rssNode->addAttribute('version', '2.0');
+        $this->rssNode->addAttribute('encoding', 'utf-8');
         $this->rssNode->addAttribute('itunes:xmlns', null, 'http://www.itunes.com/dtds/podcast-1.0.dtd');
         $this->rssNode->addAttribute('dc:xmlns', null, 'http://purl.org/dc/elements/1.1/');
 
         $this->channelNode = $this->rssNode->addChild('channel');
 
-        //Get current url
-        $this->currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") 
-        . "://$_SERVER[HTTP_HOST]" 
-        . substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/') + 1);
+        $this->getCurrentUrl();
+
+        $this->insertChannelInfos();
     }
 
+    /**
+     * @param $cwd
+     */
     public function exec($cwd)
     {
         $bulkContent = $this->getDirContent($cwd);
         $this->generateRssContent($bulkContent);
     }
 
+    /**
+     * @return array
+     */
     public function getAuthorizedTypes()
     {
         return ['mp3' => 'audio/mp3', 'mp4' => 'video/mp4', 'm4a' => 'audio/m4a', 'm4v' => 'video/m4v'];
     }
 
+    /**
+     * @param $bulkContent
+     */
     public function generateRssContent($bulkContent)
     {
         foreach($bulkContent as $item) {
@@ -69,6 +137,10 @@ class RssGenerator
         }
     }
 
+    /**
+     * @param $cwd
+     * @return array
+     */
     public function getDirContent($cwd)
     {
         $directory = array_reverse(scandir($cwd));
@@ -76,7 +148,7 @@ class RssGenerator
 
         foreach($directory as $filename) {
             $fileExtention = pathinfo($filename, PATHINFO_EXTENSION);
-            
+
             if (array_key_exists($fileExtention, $this->getAuthorizedTypes())) {
                 $bulkContent[] = ['item_name' => $filename, 'item_extension' => $this->getAuthorizedTypes()[$fileExtention]];
             }
@@ -85,11 +157,28 @@ class RssGenerator
         return $bulkContent;
     }
 
+    /**
+     * @param $itemPath
+     * @return string
+     */
     public function getItemUrl($itemPath)
     {
         return $this->currentUrl . $itemPath;
     }
 
+    /**
+     *
+     */
+    public function getCurrentUrl()
+    {
+        $this->currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http")
+            . "://$_SERVER[HTTP_HOST]"
+            . substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/') + 1);
+    }
+
+    /**
+     * @param $item
+     */
     public function insertRssItem($item)
     {
         $xmlItem = $this->channelNode->addChild('item');
@@ -106,11 +195,16 @@ class RssGenerator
             }
         }
 
-        
         $this->insertEnclosure($item, $xmlItem);
-
     }
 
+    /**
+     * @param $xmlItem
+     * @param $tagName
+     * @param $content
+     * @param array $params
+     * @param null $namespace
+     */
     public function insertFileTags($xmlItem, $tagName, $content, $params = [], $namespace = null)
     {
         $item = $xmlItem->addChild($tagName, $content, $namespace);
@@ -119,10 +213,14 @@ class RssGenerator
         }
     }
 
+    /**
+     * @param $item
+     * @return array|mixed
+     */
     public function getId3($item)
     {
         $filteredId3Infos = [];
-        $filePath = '.rss-gen-cache/' . $item['item_name'];
+        $filePath = './.rss-dependencies/.cache/' . $item['item_name'];
         $id3FilePath = $filePath . '.id3';
         $imageName = $filePath . '.png';
 
@@ -130,25 +228,28 @@ class RssGenerator
             $filteredId3Infos = unserialize(file_get_contents($id3FilePath));
         } else {
             if ($id3FileInfos = $this->ID3->analyze($item['item_name']) ?? false) {
-                if ($id3FileInfos['comments']['picture'][0]['data']) {
-                    file_put_contents($imageName, 
-                    $id3FileInfos['comments']['picture'][0]['data']);
+                if (isset($id3FileInfos['comments']['picture'][0]['data'])) {
+                    file_put_contents($imageName,
+                        $id3FileInfos['comments']['picture'][0]['data']);
+                    $finalImageName = $this->currentUrl . $imageName;
+                } else {
+                    $finalImageName = $this->config['IMAGE'];
                 }
                 $fileExtention = pathinfo($item['item_name'], PATHINFO_EXTENSION);
                 $filteredId3Infos = [
-                    "title" => !empty($id3FileInfos['tags']['id3v2']['title'][0]) 
-                    ? htmlspecialchars($id3FileInfos['tags']['id3v2']['title'][0]) 
-                    : $item['item_name'],
+                    "title" => !empty($id3FileInfos['tags']['id3v2']['title'][0])
+                        ? htmlspecialchars($id3FileInfos['tags']['id3v2']['title'][0])
+                        : $item['item_name'],
                     "pubDate" => date("D, d M y H:i:s O", filemtime($item['item_name'])),
-                    "description" => !empty($id3FileInfos['tags']['id3v2']['comment'][0]) ? 
-                    htmlspecialchars($id3FileInfos['tags']['id3v2']['comment'][0]) : " ",
+                    "description" => !empty($id3FileInfos['tags']['id3v2']['comment'][0]) ?
+                        htmlspecialchars($id3FileInfos['tags']['id3v2']['comment'][0]) : " ",
                     "link" => $id3FileInfos['tags']['id3v2']['url_user'][0] ?? null,
-                    "dc:dc:creator" => !empty($id3FileInfos['tags']['id3v2']['artist'][0]) ? 
-                    htmlspecialchars($id3FileInfos['tags']['id3v2']['artist'][0]) : "Default Creator",
+                    "dc:dc:creator" => !empty($id3FileInfos['tags']['id3v2']['artist'][0]) ?
+                        htmlspecialchars($id3FileInfos['tags']['id3v2']['artist'][0]) : "Default Creator",
                     "itunes:itunes:duration" => $id3FileInfos['playtime_string'] ?? "00:00",
-                    "itunes:itunes:author" =>  !empty($id3FileInfos['tags']['id3v2']['artist'][0]) 
-                    ? htmlspecialchars($id3FileInfos['tags']['id3v2']['artist'][0]) : "Default Creator",
-                    "itunes:itunes:image" => $this->currentUrl . $imageName,
+                    "itunes:itunes:author" =>  !empty($id3FileInfos['tags']['id3v2']['artist'][0])
+                        ? htmlspecialchars($id3FileInfos['tags']['id3v2']['artist'][0]) : "Default Creator",
+                    "itunes:itunes:image" => $finalImageName,
                     "guid" => md5($item['item_name'])
                 ];
 
@@ -157,12 +258,19 @@ class RssGenerator
                 fwrite($fileStream, serialize($filteredId3Infos));
 
                 fclose($fileStream);
+
+                /** Update last build date */
+                file_put_contents('./.rss-dependencies/.cache/date.txt', date("D, d M y H:i:s O"));
             }
         }
 
         return $filteredId3Infos;
     }
 
+    /**
+     * @param $item
+     * @param $xmlItem
+     */
     public function insertEnclosure($item, $xmlItem)
     {
         $xmlEnclosure = $xmlItem->addChild('enclosure');
@@ -171,17 +279,87 @@ class RssGenerator
         $xmlEnclosure->addAttribute('type', $item['item_extension']);
     }
 
-    public function getConfig()
+    /**
+     *
+     */
+    public function insertChannelInfos()
     {
-        $file = new SplFileObject("feed.config");
+        $lastBuildDate = file_get_contents('./.rss-dependencies/.cache/date.txt');
 
-        while ($file->valid()) {
-            $this->config[] = explode("=", trim($file->fgets()));
-        }
+        $this->channelNode->addChild('title', htmlspecialchars($this->config['TITLE']));
 
-        unset($file);
+        $this->channelNode->addChild('description', htmlspecialchars($this->config['DESCRIPTION']));
+
+        $this->channelNode->addChild('link', $this->config['LINK']);
+
+        $channelImageTag = $this->channelNode->addChild('image');
+        $channelImageTag->addChild('url', $this->config['IMAGE']);
+        $channelImageTag->addChild('title', htmlspecialchars($this->config['TITLE']));
+        $channelImageTag->addChild('link', $this->config['LINK']);
+
+        $this->channelNode->addChild('lastBuildDate', $lastBuildDate);
+
+        $channelAtomTag = $this->channelNode->addChild('atom:link');
+        $channelAtomTag->addAttribute('href', $this->currentUrl);
+        $channelAtomTag->addAttribute('rel', 'self');
+        $channelAtomTag->addAttribute('type', 'application/rss+xml');
+
+        $this->channelNode->addChild('language', $this->config['LANGUAGE']);
+
+        $this->channelNode->addChild('webMaster', htmlspecialchars($this->config['MAIL'] . ' (' . $this->config['AUTHOR'] . ')'));
+
+        $this->channelNode->addChild('language', htmlspecialchars($this->config['TITLE']));
+
+        $this->channelNode->addChild('itunes:itunes:summary', htmlspecialchars($this->config['DESCRIPTION']));
+
+        $this->channelNode->addChild('itunes:itunes:subtitle', htmlspecialchars($this->config['SUBTITLE']));
+
+        $this->channelNode->addChild('itunes:itunes:explicit', $this->config['EXPLICIT']);
+
+        $this->channelNode->addChild('itunes:itunes:author', htmlspecialchars($this->config['AUTHOR']));
+
+        $this->channelNode->addChild('itunes:itunes:image')
+            ->addAttribute('href', $this->config['IMAGE']);
+
+        $channelOwnerTag = $this->channelNode->addChild('itunes:owner');
+        $channelOwnerTag->addChild('itunes:itunes:name', htmlspecialchars($this->config['AUTHOR']));
+        $channelOwnerTag->addChild('itunes:itunes:email', htmlspecialchars($this->config['MAIL']));
+
+        $channelICategoryTag = $this->channelNode->addChild('itunes:itunes:category');
+        $channelICategoryTag->addAttribute('text', htmlspecialchars($this->config['CATEGORY']));
+
+        $channelCopyTag = $this->channelNode;
+        $channelCopyTag->addChild('copyright', date("Y") . ' ' . htmlspecialchars($this->config['LICENCE']) . ' ' . htmlspecialchars($this->config['AUTHOR']));
+
+        $this->channelNode->addChild('generator', "Podcast RSS Generator From Folder (or PRGFF if you want to splutter)");
     }
 
+    /**
+     *
+     */
+    public function getConfig()
+    {
+        try {
+            $file = new SplFileObject("./.rss-dependencies/channel.config");
+
+            while ($file->valid()) {
+                $configBuffer = explode("=", trim($file->fgets()));
+
+                if (count($configBuffer) == 2) {
+
+                    $this->config[$configBuffer[0]] = $configBuffer[1];
+                }
+            }
+
+            unset($file);
+        } catch (Exception $error) {
+            echo "Error on config loading " . $error;
+        }
+    }
+
+    /**
+     *
+     */
     public function displayRss()
     {
         print($this->rssNode->asXML());
